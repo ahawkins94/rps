89f90dcc9a0e38c79b341c9bb5fee822a2ba0569
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-11 17:48:27 +0100

    added the comparison nested if statement for the first, will copy and paste for the other two statements and then tidy up the strings

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ var weapon = prompt('Choose your weapon!!!!!!!!\n r for rock\n p for paper\ns fo
 var cpuWeapon = Math.random();
 
 if (cpuWeapon < 0.34) {
-	cpuWeapon = 'rock';
+	cpuWeapon = 'r';
 } else if (cpuWeapon <= 0.67) {
-	cpuWeapon = 'paper';
+	cpuWeapon = 'p';
 } else {
-	cpuWeapon = 'scizzors';
+	cpuWeapon = 's';
 }
 
 console.log(cpuWeapon)
 
+if (weapon === cpuWeapon) {
+
+	alert('It is a draw!!')
+}
+
+if(weapon === 'r'){
+
+    if(cpuWeapon === 's'){
+
+    	 alert('rock wins');
+        }
+        else if (cpuWeapon === 'p') {
+            alert('paper wins');
+        }
+}
 
